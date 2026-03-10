a11b660f9d8fdf4f81733c6079674ac2289c0056
Kevin Rama <kevinrama181205@gmail.com>
2026-03-10 15:48:56 -0400

    k

---
+++ ex6/ft_analytics_dashboard.py
@@ -0,0 +1,27 @@
+if __name__ == "__main__":
+    players = [
+        {
+            "player": "alice",
+            "score": 2300,
+            "achievements": ["first_kill", "level_10", "boss_slayer", "speedrun", "pacifist"],
+            "region": "north"
+        },
+        {
+            "player": "bob",
+            "score": 1800,
+            "achievements": ["first_kill", "level_10", "boss_slayer"],
+            "region": "east"
+        },
+        {
+            "player": "charlie",
+            "score": 2150,
+            "achievements": ["first_kill", "level_10", "boss_slayer", "speedrun", "pacifist", "no_damage", "collector"],
+            "region": "central"
+        },
+        {
+            "player": "diana",
+            "score": 2300,
+            "achievements": ["first_kill", "level_10"],
+            "region": "north"
+        },
+    ]
